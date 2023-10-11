b68ef5605b96e94591d22727f9cf164aa3da8a6d
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-10-11 15:34:55 +0200

    trigger tmdb search from search bar

--- app/src/main/java/com/example/thewatchlist/ui/components/DetailedInfo.kt
+++ app/src/main/java/com/example/thewatchlist/ui/components/DetailedInfo.kt
@@ -1,20 +1,13 @@
 package com.example.thewatchlist.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
@@ -32,9 +25,6 @@ import androidx.compose.ui.unit.sp
 import com.example.thewatchlist.data.media.Media
 import com.example.thewatchlist.data.media.Movie
 import com.example.thewatchlist.data.media.TV
-import com.example.thewatchlist.data.navigation.MainNavOption
-import info.movito.themoviedbapi.TmdbTvSeasons
-import org.w3c.dom.Text
 
 @Composable
 fun DetailedInfo(media: Media) {
--- app/src/main/java/com/example/thewatchlist/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/thewatchlist/ui/screens/SearchScreen.kt
@@ -3,9 +3,7 @@ package com.example.thewatchlist.ui.screens
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -21,7 +19,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.thewatchlist.data.navigation.MainNavOption
 import com.example.thewatchlist.network.SearchStatus
@@ -29,7 +26,6 @@ import com.example.thewatchlist.ui.DataViewModel
 import com.example.thewatchlist.ui.components.Banner
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     dataViewModel: DataViewModel,
