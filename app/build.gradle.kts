6f1078586ac3d2f0d0b8fb10626948e0adb69a70
marthsja <127052202+marthsja@users.noreply.github.com>
2023-10-11 14:35:04 +0200

    searchbar without complete functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.3")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation ("androidx.compose.material3:material3:1.2.0-alpha09")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/thewatchlist/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/thewatchlist/ui/screens/SearchScreen.kt
@@ -1,11 +1,23 @@
 package com.example.thewatchlist.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -14,18 +26,75 @@ import com.example.thewatchlist.network.SearchStatus
 import com.example.thewatchlist.ui.DataViewModel
 import com.example.thewatchlist.ui.components.Banner
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     dataViewModel: DataViewModel,
     mainNavController: NavController
 ) {
 
-    LaunchedEffect(Unit) {
-        dataViewModel.searchTmdb("avatar")
-    }
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+//    var search by remember { mutableStateOf(false) }    // activate search
+
 
     Column {
-        Text(text = "online søk for 'avatar':")
+
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = text,
+            onQueryChange = { text = it },
+            onSearch = {
+
+                // Need to search for movie/series here
+//                search = true
+
+
+                active = false
+            },
+            active = active,
+            onActiveChange = { active = it },
+            placeholder = { Text(text = "Search") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            },
+            trailingIcon = {
+                if(active) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            if(text.isNotEmpty()) {
+                                text = ""
+                            } else {
+                                active = false
+                            }
+                        },
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close Icon"
+                    )
+                }
+            },
+
+            ) {
+
+        }
+
+/*
+        // Searches once with the input,
+        // but doesn't run again if we want to search for something else
+        if(search) { LaunchedEffect(Unit) { dataViewModel.searchTmdb(text) } }
+*/
+
+
+        // Searches only once when the app first starts, therefore searches for nothing
+        LaunchedEffect(Unit) {
+            dataViewModel.searchTmdb(text)
+        }
+
+        Text(text = "online søk for '$text':")
         Spacer(modifier = Modifier.padding(bottom = 10.dp))
         when (val res = dataViewModel.searchStatus) {
             is SearchStatus.Success -> { res.results.forEach {
@@ -37,7 +106,7 @@ fun SearchScreen(
                         mainNavController.navigate("details")
                     },
                     onAdd = { dataViewModel.addMediaToList(it) }
-                    )
+                )
             } }
             is SearchStatus.Loading -> Loading()
             is SearchStatus.Error -> Error()
